512e2eb657954f7140f62b0c5b6c7c5278df89ed
herter4171 <jwhh91@gmail.com>
2020-07-11 17:28:56 -0400

    Working wx.Choice

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-from mats_app import mats_app
+import wx
 
-app = mats_app()
+
+class Mywin(wx.Frame):
+    def __init__(self, parent, title):
+        super(Mywin, self).__init__(parent, title=title, size=(300, 100))
+
+        panel = wx.Panel(self)
+        box = wx.BoxSizer(wx.VERTICAL)
+
+        languages = ['C', 'C++', 'Python', 'Java', 'Perl']
+
+        self.choice = wx.Choice(panel, choices=languages)
+        box.Add(self.choice, 1, wx.EXPAND | wx.ALL, 5)
+
+        box.AddStretchSpacer()
+
+        panel.SetSizer(box)
+        self.Centre()
+        self.Show()
+
+
+app = wx.App()
+Mywin(None, 'ComboBox and Choice demo')
 app.MainLoop()
